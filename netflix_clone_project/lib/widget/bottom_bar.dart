f4d5105b6a3ad684b1272d72abe70ea689400c8b
Enterprise09 <goldenstaytes@gmail.com>
2021-06-04 21:39:06 +0900

    Create bottom_bar

---
+++ netflix_clone_project/lib/widget/bottom_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Container(
+        height: 50,
+        child: TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.transparent,
+          indicatorColor: Colors.transparent,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(
+                Icons.home,
+                size: 18,
+              ),
+              child: Text(
+                "홈",
+                style: TextStyle(fontSize: 9),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
