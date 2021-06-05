1e34a315a5c249dd8a742b06704da89feb94a76f
Enterprise09 <goldenstaytes@gmail.com>
2021-06-05 22:54:06 +0900

    Create for Display Movie Info

---
+++ netflix_clone_project/lib/screen/detail_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_project/model/model_movie.dart';
+import 'dart:ui';
+
+class DetailScreen extends StatefulWidget {
+  final Movie movie;
+  DetailScreen({required this.movie});
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  bool like = false;
+
+  @override
+  void initState() {
+    super.initState();
+    like = widget.movie.like;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/' + widget.movie.poster),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
