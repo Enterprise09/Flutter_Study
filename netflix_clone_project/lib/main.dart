8c3d2371a19b53db79364038f1b29a3a04dca945
Enterprise09 <goldenstaytes@gmail.com>
2021-06-06 20:51:13 +0900

    Add Profile View Page

--- netflix_clone_project/lib/main.dart
+++ netflix_clone_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_project/screen/home_screen.dart';
+import 'package:netflix_clone_project/screen/more_screen.dart';
 import 'package:netflix_clone_project/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -37,11 +38,7 @@ class _MyAppState extends State<MyApp> {
                   child: Text('Save'),
                 ),
               ),
-              Container(
-                child: Center(
-                  child: Text('More'),
-                ),
-              ),
+              MoreScreen(),
             ],
           ),
           bottomNavigationBar: Bottom(),
