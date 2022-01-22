9c585cc201586f71b9ac35683e365e779407cdd5
Enterprise09 <goldenstaytes@gmail.com>
2022-01-22 18:55:08 +0900

    [+] review the sample code

--- basic_flutter/lib/main.dart
+++ basic_flutter/lib/main.dart
@@ -5,12 +5,58 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'hello Demo',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const MyHome(
+        title: 'hello Flutter!',
+      ),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  int _count = 0;
+  void _incrementCounter() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('버튼을 클릭해보세요!'),
+              Text(
+                '$_count',
+                style: Theme.of(context).textTheme.headline5,
+              )
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: '카운팅!',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
